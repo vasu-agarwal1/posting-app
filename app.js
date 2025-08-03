b0b8a1978568fabaa01e7ef105080b3239e31135
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-08-03 23:41:23 +0530

    rendered login page and applied jwt

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const userModel = require("./models/user")
+const postModel = require("./models/post")
 const cookieParser = require('cookie-parser')
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 app.set("view engine", "ejs")
 app.use(express.json())
@@ -11,6 +13,15 @@ app.use(cookieParser())
 
 app.get("/", (req,res)=>{
     res.render("index")
+})
+
+app.get("/login", (req,res) => {
+    res.render("login")
+})
+
+app.get("/logout", (req, res)=>{
+    res.cookie("token", "")
+    res.redirect("/login")
 })
 
 app.post("/register",async (req, res) => {
@@ -28,8 +39,30 @@ app.post("/register",async (req, res) => {
                 name,
                 password: hash
             })
+
+            let token = jwt.sign({email, userid: user._id}, "shhh")
+            res.cookie("token", token)
+            res.send("registered")
         } )
     })
 })
+
+app.post("/login",async (req, res) => {
+    let {email , password} = req.body
+
+    let user = await userModel.findOne({email})
+    if(!user) return res.status(500).send("something went wrong")
+
+    bcrypt.compare(password, user.password, (err, result) => {
+        if(result) res.status(200).send("you can login")
+        else  res.redirect("/login")
+    })
+
+})
+
+function isLoggedIn(req, res, next){
+    if(req.cookies.token === "") res.send("you must be logged in")
+    next()
+}
 
 app.listen(3000)
